3c116cf85970296d2df8a888925adbc6307c6d41
Rana <reduanulislam92665@gmail.com>
2025-10-03 19:53:44 +0600

    update upload middleweare

--- src/middlewares/upload.middleware.ts
+++ src/middlewares/upload.middleware.ts
@@ -1,9 +1,17 @@
 import multer from "multer";
 import path from "path";
+import fs from "fs";
+
+const uploadDir = path.join(__dirname, "../../uploads");
+
+// Ensure uploads folder exists
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir, { recursive: true });
+}
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "uploads/"); // folder inside your project
+    cb(null, uploadDir);
   },
   filename: (req, file, cb) => {
     cb(null, Date.now() + path.extname(file.originalname));
