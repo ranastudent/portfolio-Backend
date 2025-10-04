9f23abf51c0507774786cbe5ca20b8dfa6ffa517
Rana <reduanulislam92665@gmail.com>
2025-10-04 19:47:20 +0600

    update project validation

--- src/modules/project/project.validation.ts
+++ src/modules/project/project.validation.ts
@@ -4,7 +4,7 @@ export const createProjectSchema = z.object({
   title: z.string().min(3, "Title must be at least 3 characters"),
   description: z.string().min(10, "Description must be at least 10 characters"),
   features: z.array(z.string()).min(1, "At least one feature is required"),
-  thumbnail: z.string().url().optional(),
+  thumbnail: z.string().optional(),
   liveLink: z.string().url().optional(),
   repoLink: z.string().url().optional(),
 });
