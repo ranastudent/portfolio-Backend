a318ff111a0ee0ff7554e868391976af3bd6967c
Rana <reduanulislam92665@gmail.com>
2025-10-05 19:29:59 +0600

    update blog.validation

--- src/modules/blog/blog.validation.ts
+++ src/modules/blog/blog.validation.ts
@@ -2,7 +2,7 @@ import { z } from "zod";
 
 export const createBlogSchema = z.object({
   title: z.string().min(3, "Title must be at least 3 characters"),
-  description: z.string().min(10, "Description must be at least 10 characters"),
+  
   content: z.string().min(20, "Content must be at least 20 characters"),
   tags: z.array(z.string()).optional(),
 });
