28976a17b0b673e8dc110e2a7f92f4d1bedaea25
Rana <reduanulislam92665@gmail.com>
2025-10-06 22:24:05 +0600

    update blog validation

--- src/modules/blog/blog.validation.ts
+++ src/modules/blog/blog.validation.ts
@@ -9,7 +9,7 @@ export const createBlogSchema = z.object({
 
 export const updateBlogSchema = z.object({
   title: z.string().min(3).optional(),
-  description: z.string().min(10).optional(),
+  description: z.string().min(10).optional().or(z.literal("")), // allow empty
   content: z.string().min(20).optional(),
   tags: z.array(z.string()).optional(),
 });
