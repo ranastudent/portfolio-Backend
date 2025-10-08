e9c50ba0b9e5abe7eb4272a55bd579583ccfdb52
Rana <reduanulislam92665@gmail.com>
2025-10-08 07:07:41 +0600

    update app.ts

--- src/app.ts
+++ src/app.ts
@@ -10,10 +10,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: [
-      "http://localhost:3000", 
-      "http://localhost:3001"
-    ],
+    origin: "*",
     credentials: true,
   })
 );
